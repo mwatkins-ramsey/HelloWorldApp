61c0162b313c0059f914cb2fedce2794c87ac960
Michael Watkins <130600163+mwatkins-ramsey@users.noreply.github.com>
2023-07-19 15:56:15 -0500

    Is now an API

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@
 
 COPY . /src
 WORKDIR "/src"  
-
-  
-RUN  dotnet build "HelloWorldApp.sln" && dotnet test "HelloWorldApp.sln"  && dotnet publish "HelloWorldApp/HelloWorldApp.csproj" -c Release -o /src/out
+ARG CACHEBUST=1
+RUN  dotnet restore "HelloWorldApp.sln" &&  \
+     dotnet build "HelloWorldApp.sln" &&  \
+     dotnet test "HelloWorldApp.sln"  &&  \
+     dotnet publish "HelloWorldApp/HelloWorldApp.csproj" -c Release -o /src/out
      #&& dotnet publish "HelloWorldApp/HelloWorldApp.csproj"
   
 # Build runtime image  
 FROM mcr.microsoft.com/dotnet/aspnet:7.0 as prod
+ENV API_PORT=5000
 WORKDIR /src  
 COPY --from=builder /src/out .  
 ENTRYPOINT ["dotnet", "HelloWorldApp.dll"]
--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,12 +1,54 @@
 using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace HelloWorldApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var port = GetPortFromEnvironment() ?? 5000;
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.ListenAnyIP(port);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // Add routing services
+                    services.AddRouting();
+                })
+                .Configure(app =>
+                {
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/hello", async context =>
+                        {
+                            context.Response.ContentType = "application/json";
+                            await context.Response.WriteAsync("{\"message\":\"Hello World\"}");
+                        });
+                    });
+                })
+                .Build();
+
+            host.Run();
+        }
+        
+        private static int? GetPortFromEnvironment()
+        {
+            var portString = Environment.GetEnvironmentVariable("API_PORT");
+            if (int.TryParse(portString, out int port))
+            {
+                return port;
+            }
+            Console.WriteLine("API_PORT not set in env, resorting to default");
+            return null;
         }
     }
 }
